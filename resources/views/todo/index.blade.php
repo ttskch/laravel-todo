0e5b5907b35dbd81b1bff9c76b5efd24d25b5071
Takashi Kanemoto <ttskch@gmail.com>
2020-05-17 15:49:29 +0900

    fix: 🐛 show errors

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -23,6 +23,15 @@
                     </div>
                 </div>
             </form>
+            @if ($errors->any())
+                <div class="alert alert-danger mt-3 mb-0">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </li>
     </ul>
 @endsection
