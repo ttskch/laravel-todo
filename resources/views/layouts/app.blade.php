a21cd325e424a96182e16e4edfe93facd27e57b8
Takashi Kanemoto <ttskch@gmail.com>
2020-05-17 15:44:16 +0900

    feat: 🎸 use bootstrap and fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,31 @@
+<!doctype html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
     <title>Laravel Todo</title>
 </head>
-<body>
-@yield('content')
+<body class="bg-light">
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('todo_index') }}">Laravel Todo</a>
+    </div>
+</nav>
+
+<div id="content" class="bg-white py-3">
+    <div class="container">
+        @yield('content')
+    </div>
+</div>
+
+<footer class="container py-3">
+    <div class="small text-muted text-right">
+        <a href="http://ttskch.github.io" target="_blank" class="text-muted"><b>ttskch</b></a> &copy; {{ date('Y') }}
+    </div>
+</footer>
+
+<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,24 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <ul>
+    <ul class="list-group">
         @foreach ($todos as $todo)
-            <li>{{ $todo->name }} {{ $todo->isDone ? 'done' : 'undone' }}</li>
+            <li class="list-group-item">
+                @if ($todo->isDone)
+                    <s class="text-muted">{{ $todo->name }}</s>
+                    <span class="badge badge-success float-right">Done</span>
+                @else
+                    {{ $todo->name }}
+                @endif
+            </li>
         @endforeach
-    </ul>
 
-    <form action="{{ route('todo_new') }}" method="post">
-        @csrf
-        <input type="text" name="name" required>
-        <button type="submit">Add</button>
-    </form>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        <li class="list-group-item px-3">
+            <form action="{{ route('todo_new') }}" method="post">
+                @csrf
+                <div class="input-group">
+                    <input type="text" name="name" class="form-control" required>
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-outline-secondary"><i class="fa fa-plus"></i> Add</button>
+                    </div>
+                </div>
+            </form>
+        </li>
+    </ul>
 @endsection
