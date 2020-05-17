439a2de1834c64a221df28d7630aab4b0e114990
Takashi Kanemoto <ttskch@gmail.com>
2020-05-17 14:46:38 +0900

    feat: 🎸 enable to add todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Collective\Annotations\Routing\Annotations\Annotations\Get;
+use Collective\Annotations\Routing\Annotations\Annotations\Middleware;
+use Collective\Annotations\Routing\Annotations\Annotations\Post;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
     /**
      * @Get("/", as="todo_index")
+     * @Middleware("web")
      */
     public function index()
     {
@@ -17,5 +21,20 @@ class TodoController extends Controller
         return view('todo.index', [
             'todos' => $todos,
         ]);
+    }
+
+    /**
+     * @Post("/new", as="todo_new")
+     * @Middleware("web")
+     */
+    public function new(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        DB::insert('insert into todos (name, isDone) values (?, ?)', [$validatedData['name'], 0]);
+
+        return redirect(route('todo_index'));
     }
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -6,4 +6,19 @@
             <li>{{ $todo->name }} {{ $todo->isDone ? 'done' : 'undone' }}</li>
         @endforeach
     </ul>
+
+    <form action="{{ route('todo_new') }}" method="post">
+        @csrf
+        <input type="text" name="name" required>
+        <button type="submit">Add</button>
+    </form>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @endsection
